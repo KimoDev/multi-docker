9f6ca104a77bf874a8cd0499912b2a06c6b5fdb9
Kimo <josephmarquanddesigns@gmail.com>
2020-09-24 14:13:12 +0100

    fixed pgclient user key typo

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 // Postgres Client Setup
 const { Pool } = require('pg');
 const pgClient = new Pool({
-  user: keys.user,
+  user: keys.pgUser,
   host: keys.pgHost,
   database: keys.pgDatabase,
   password: keys.pgPassword,
